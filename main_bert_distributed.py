bb34afe87e006cfc1a2d00fa46efb23384e0bcbe
Oleg Boiko <oleh.boiko@gmail.com>
2020-10-31 15:35:49 -0700

    Making main_bert_distributed plain python script

--- main_bert_distributed.py
+++ main_bert_distributed.py
@@ -1,17 +1,21 @@
 import os
 import torch
 from torch import nn
+import shutil
 from tqdm import tqdm
 from dataset import Oxford2019Dataset
 from torch.utils.data import DataLoader
 from typing import Callable
 from transformers import AdamW
 from torch import nn
+from loguru import logger
 from transformers import (EncoderDecoderModel,
                           PreTrainedModel,
                           BertTokenizer,
                           BertGenerationEncoder,
                           BertGenerationDecoder)
+
+logger.add("log.txt")
 
 if torch.cuda.is_available():
     main_device = torch.device("cuda:0")
@@ -51,7 +55,6 @@ test_set = make_data_loader('test.txt')
 valid_set = make_data_loader('valid.txt')
 
 
-
 def run(model: nn.Module,
         data_loader: DataLoader,
         tokenizer: BertTokenizer,
@@ -82,8 +85,6 @@ def run(model: nn.Module,
     return loss
 
 
-
-
 def train(epochs: int, train_data_loader: DataLoader, valid_data_loader: DataLoader = None, model: nn.Module = None):
     if model is None:
         encoder = BertGenerationEncoder.from_pretrained(model_type,
@@ -110,7 +111,7 @@ def train(epochs: int, train_data_loader: DataLoader, valid_data_loader: DataLoa
         optimizer.zero_grad()
 
         if i % 10 == 0:
-            print(f'batch_error={batch_loss.item()};')
+            logger.info(f'batch_error={batch_loss.item()};')
 
     for i in range(epochs):
         model.train()
@@ -123,22 +124,19 @@ def train(epochs: int, train_data_loader: DataLoader, valid_data_loader: DataLoa
         else:
             val_loss = 'N/A'
 
-        msg = f'train_error={train_loss};  valid_error={val_loss};'
-        print(msg)
-        get_ipython().system("echo '{msg}' >> log.txt")
+        logger.info(f'train_error={train_loss};  valid_error={val_loss};')
     return model
 
 
 def save_model(model: PreTrainedModel):
-    out_loc = '/opt/ml/model' if is_sagemaker_estimator else '.'
-    get_ipython().system('mkdir -p {out_loc}')
+    out_loc = '/opt/ml/model'
+    os.makedirs(out_loc, exist_ok=True)
 
     model.save_pretrained(out_loc)
 
-    get_ipython().system('cp main_bert.py {out_loc}')
-    get_ipython().system('cp main_bert.ipynb {out_loc}')
-    get_ipython().system('cp log.txt {out_loc}')
+    shutil.copyfile(__file__, os.path.join(out_loc, __file__))
+    shutil.copyfile('log.txt', os.path.join(out_loc, 'log.txt'))
 
 
-model = train(epochs=epochs, train_data_loader=valid_set, valid_data_loader=test_set)
+model = train(epochs=epochs, train_data_loader=test_set, valid_data_loader=valid_set)
 save_model(model)
